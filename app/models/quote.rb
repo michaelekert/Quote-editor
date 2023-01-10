d5964ce424bb7dc02ffe07de56ab47c91da9557b
Michael Ekert <ekert.michael@gmail.com>
2023-01-10 21:59:09 +0100

    feat: Update actions turbo in real time

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,11 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, ->{order(id: :desc)}
+
+
+  after_create_commit -> {broadcast_prepend_later_to  "quotes"}
+  after_update_commit -> {broadcast_replace_later_to  "quotes"}
+  after_destroy_commit -> {broadcast_remove_to "quotes"}
+
+  # broadcast_to -> (quote)
 end
