1e9ae1adb64af9c3da75b2a73b66c2ab3b04cd83
Michael Ekert <ekert.michael@gmail.com>
2023-01-15 17:41:27 +0100

    feat: Add CRUD actions for line_item_date

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'line_item_dates/new'
+  get 'line_item_dates/create'
+  get 'line_item_dates/edit'
+  get 'line_item_dates/update'
+  get 'line_item_dates/destroy'
   root to: "pages#home"
   devise_for :users
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:show, :index]
+  end
 end
