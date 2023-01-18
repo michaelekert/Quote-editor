b19e4a88dd39d602abbf75de53c914b143f69439
Michael Ekert <ekert.michael@gmail.com>
2023-01-18 12:50:41 +0100

    feat: Add update action via turbo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'line_item_dates/new'
-  get 'line_item_dates/create'
-  get 'line_item_dates/edit'
-  get 'line_item_dates/update'
-  get 'line_item_dates/destroy'
   root to: "pages#home"
   devise_for :users
   resources :quotes do
