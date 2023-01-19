67389e8308b9775de05252ad04568b7fda6fe838
Michael Ekert <ekert.michael@gmail.com>
2023-01-19 13:41:52 +0100

    routes: Add line item routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
   resources :quotes do
-    resources :line_item_dates, except: [:show, :index]
+    resources :line_item_dates, except: [:show, :index] do
+      resources :line_item, except: [:show, :index]
+    end
   end
 end
