6e74d31a90fa037d290dfdae20cf85b80bfb169c
Michael Ekert <ekert.michael@gmail.com>
2023-01-19 12:46:22 +0100

    test: Add tests to line item dates

---
+++ test/system/line_item_dates_test.rb
@@ -0,0 +1,49 @@
+require 'application_system_test_case'
+
+class LineItemDates < ApplicationSystemTestCase
+  setup do
+    login_as users(:accountant)
+
+    @quote = quotes(:first)
+    @line_item_date = line_item_dates(:today)
+
+    visit quote_path(@quote)
+  end
+
+  test "Creatin a new line item date" do
+    assert_selector "h1", text: "First quote"
+
+    click_on "New Date"
+    assert_selector "h1", text: "First quote"
+    fill_in "Date", with: Date.current + 1.day
+
+    click_on "Create Date"
+    assert_text I18n.l(Date.current + 1.day, format: :long)
+
+  end
+
+  test "Updating a line item date" do
+    assert_selector "h1", text: "First quote"
+
+    within id: dom_id(@line_item_date) do
+      click_on "Edit"
+    end
+
+    assert_selector "h1", text: "First quote"
+    fill_in "Date", with: Date.current + 1.day
+    click_on "Update Date"
+    assert_text I18n.l(Date.current + 1.day, format: :long)
+  end
+
+  test "Destroying a line item date" do
+    assert_selector "h1", text: "First quote"
+    assert_text I18n.l(Date.current, format: :long)
+    accept_confirm do
+      within id: dom_id(@line_item_date) do
+        click_on "Delete"
+      end
+    end
+    assert_no_text I18n.l(Date.current, format: :long)
+  end
+
+end
