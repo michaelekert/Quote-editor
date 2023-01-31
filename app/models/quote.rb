d2aebf1edf0702804743bd2592c8892984b247c9
Michael Ekert <ekert.michael@gmail.com>
2023-01-31 15:38:09 +0100

    Finish project

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,10 +2,15 @@ class Quote < ApplicationRecord
   has_many :line_item_dates, dependent: :destroy
   belongs_to :company
   validates :name, presence: true
+  has_many :line_items, through: :line_item_dates
 
   scope :ordered, ->{order(id: :desc)}
 
 
 
    broadcasts_to -> (quote) {[quote.company, "quotes"]}, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
--- test/system/line_item_dates_test.rb
+++ test/system/line_item_dates_test.rb
@@ -1,6 +1,8 @@
 require 'application_system_test_case'
 
 class LineItemDates < ApplicationSystemTestCase
+
+  include ActionView::Helpers::NumberHelper
   setup do
     login_as users(:accountant)
 
@@ -25,7 +27,7 @@ class LineItemDates < ApplicationSystemTestCase
   test "Updating a line item date" do
     assert_selector "h1", text: "First quote"
 
-    within id: dom_id(@line_item_date) do
+    within id: dom_id(@line_item_date, :edit) do
       click_on "Edit"
     end
 
@@ -39,11 +41,13 @@ class LineItemDates < ApplicationSystemTestCase
     assert_selector "h1", text: "First quote"
     assert_text I18n.l(Date.current, format: :long)
     accept_confirm do
-      within id: dom_id(@line_item_date) do
+      within id: dom_id(@line_item_date, :edit) do
         click_on "Delete"
       end
     end
     assert_no_text I18n.l(Date.current, format: :long)
+
+    assert_text number_to_currency(@quote.total_price)
   end
 
 end
